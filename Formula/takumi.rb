59be4e4a85ed56cec62631cb80dae5406192fa23
Kaiyi Wong <kaiyiwong@gmail.com>
2026-04-05 22:18:54 +0900

    Use tagged release v1.0.0 instead of main branch

--- Formula/takumi.rb
+++ Formula/takumi.rb
@@ -1,9 +1,8 @@
 class Takumi < Formula
   desc "The craftsman's toolkit for shaping video assets"
   homepage "https://github.com/kaiyiwong/takumi"
-  url "https://github.com/kaiyiwong/takumi/archive/refs/heads/main.tar.gz"
-  version "1.0.0"
-  sha256 "35d4e7ef1156203b73537b0cab73a7c0246b9b723330304863091df7a45a1d8f"
+  url "https://github.com/kaiyiwong/takumi/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "05a84c63f234a5f4fe87a700bbf922aa1b7094271359912b286de67e079cdd7e"
   license "MIT"
 
   depends_on "ffmpeg"
